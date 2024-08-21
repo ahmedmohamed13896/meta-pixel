a057ebb59d286ebdbb5710e07f936db26f1d1708
hagar.mohamed <hmohamed-v@ejada.com>
2024-08-21 18:14:53 +0300

    feat: created project details page without video

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,6 +1,7 @@
 const NAV_BAR = document.querySelector('.navbar');
 const HEADER = document.querySelector('.header-wrapper');
-const HEADER_WORKS = document.querySelector('.work-details');
+const SECTION_WORKS = document.querySelector('.work-details');
+const SECTION_PROJECT_DETAILS = document.querySelector('.project-wrap');
 const SECTIONS = document.querySelectorAll('section');
 const BULLETS = document.querySelectorAll('.bullet');
 const HALF_WINDOW_HEIGHT = window.innerHeight / 2;
