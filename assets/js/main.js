c5d716a1690071ceb0e1ffbce86f74fe041dcd01
hagar.mohamed <hmohamed-v@ejada.com>
2024-09-12 00:08:37 +0300

    feat: added projects images in home page and added hover feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,102 +1,144 @@
-window.onload = function() {
-    LOADER.style.display =  'none';
+window.onload = function () {
+  LOADER.style.display = "none";
 };
-
 
 /////////////////
 
 function updateActiveBullet() {
-    SECTIONS.forEach((section, index) => {
-        const rect = section.getBoundingClientRect();
-        const bullet = BULLETS[index];
-        if (rect.top <= HALF_WINDOW_HEIGHT) {
-            bullet.classList.add('active');
-        } else {
-            bullet.classList.remove('active');
-        }
-    });
+  SECTIONS.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    const bullet = BULLETS[index];
+    if (rect.top <= HALF_WINDOW_HEIGHT) {
+      bullet.classList.add("active");
+    } else {
+      bullet.classList.remove("active");
+    }
+  });
 }
 
 function updateNavbarStyle(navHeight) {
-    const isExpanded = NAV_COLLAPSE.classList.contains('show');    
-    if ((window.scrollY >= navHeight) || isExpanded) {
-        NAV_BAR.classList.add('fadeInBackground');
-    } else {
-        NAV_BAR.classList.remove('fadeInBackground');
-    }
+  const isExpanded = NAV_COLLAPSE.classList.contains("show");
+  if (window.scrollY >= navHeight || isExpanded) {
+    NAV_BAR.classList.add("fadeInBackground");
+  } else {
+    NAV_BAR.classList.remove("fadeInBackground");
+  }
 }
 
 function onScroll(isUpdateBullet) {
-    // const headerHeight = HEADER?.offsetHeight || SECTION_WORKS?.offsetHeight || SECTION_PROJECT_DETAILS?.offsetHeight || SECTION_SERVICES_DETAILS?.offsetHeight || SECTION_ABOUT?.offsetHeight;
-    updateNavbarStyle(NAV_HEIGHT);
-    if (HEADER) {
-        isUpdateBullet && updateActiveBullet();
-    }
+  // const headerHeight = HEADER?.offsetHeight || SECTION_WORKS?.offsetHeight || SECTION_PROJECT_DETAILS?.offsetHeight || SECTION_SERVICES_DETAILS?.offsetHeight || SECTION_ABOUT?.offsetHeight;
+  updateNavbarStyle(NAV_HEIGHT);
+  if (HEADER) {
+    isUpdateBullet && updateActiveBullet();
+  }
 }
 
 onScroll(); // to update navbar if the user on different sectiom
 
-document.addEventListener('scroll', function () {
-    onScroll(true);
+document.addEventListener("scroll", function () {
+  onScroll(true);
 });
 
-NAV_TOGGLER.addEventListener('click',function(e){
-    if(e.target.classList.contains('show')){
-        NAV_BAR.classList.add('fadeInBackground');
-    }else if (window.scrollY < NAV_HEIGHT){
-        NAV_BAR.classList.add('fadeInBackground');
-    }
+NAV_TOGGLER.addEventListener("click", function (e) {
+  if (e.target.classList.contains("show")) {
+    NAV_BAR.classList.add("fadeInBackground");
+  } else if (window.scrollY < NAV_HEIGHT) {
+    NAV_BAR.classList.add("fadeInBackground");
+  }
 });
 
-
-if(BUTTON_SCROLL_DOWN){
-    BUTTON_SCROLL_DOWN.addEventListener('click', function(){
-        window.scrollTo({
-            top: FULL_WINDOW_HEIGHT, 
-            behavior: 'smooth'
-        });
-    })
+if (BUTTON_SCROLL_DOWN) {
+  BUTTON_SCROLL_DOWN.addEventListener("click", function () {
+    window.scrollTo({
+      top: FULL_WINDOW_HEIGHT,
+      behavior: "smooth",
+    });
+  });
 }
 
 // JavaScript to handle multi-select filter in Work.html page
-if(FILTER_ITEMS?.length){
-    FILTER_ITEMS.forEach(tab => {
-        
-        tab.addEventListener('click', () => {
-            const filter = tab.getAttribute('data-filter');
-    
-            // Toggle selected state
-            if (tab.classList.contains('selected')) {
-                tab.classList.remove('selected');
-                SELECTED_FILTERS = SELECTED_FILTERS.filter(f => f !== filter);
-            } else {
-                tab.classList.add('selected');
-                SELECTED_FILTERS.push(filter);
-            }
-    
-            // Show or hide content based on selected filters
-            BOXS_CONTENT.forEach(content => {
-                const contentClasses = content.classList;
-    
-                // Show content if it matches any of the selected filters
-                if (SELECTED_FILTERS.some(f => contentClasses.contains(f))) {
-                    content.classList.add('show');
-                } else {
-                    content.classList.remove('show','aos-animate');
-                }
-            });
-    
-            // If no filters are selected, show all content
-            if (SELECTED_FILTERS.length === 0) {
-                BOXS_CONTENT.forEach(content => content.classList.add('show'));
-            }
+if (FILTER_ITEMS?.length) {
+  FILTER_ITEMS.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      const filter = tab.getAttribute("data-filter");
 
-            AOS.init();
-        });
+      // Toggle selected state
+      if (tab.classList.contains("selected")) {
+        tab.classList.remove("selected");
+        SELECTED_FILTERS = SELECTED_FILTERS.filter((f) => f !== filter);
+      } else {
+        tab.classList.add("selected");
+        SELECTED_FILTERS.push(filter);
+      }
+
+      // Show or hide content based on selected filters
+      BOXS_CONTENT.forEach((content) => {
+        const contentClasses = content.classList;
+
+        // Show content if it matches any of the selected filters
+        if (SELECTED_FILTERS.some((f) => contentClasses.contains(f))) {
+          content.classList.add("show");
+        } else {
+          content.classList.remove("show", "aos-animate");
+        }
+      });
+
+      // If no filters are selected, show all content
+      if (SELECTED_FILTERS.length === 0) {
+        BOXS_CONTENT.forEach((content) => content.classList.add("show"));
+      }
+
+      AOS.init();
     });
+  });
 }
 
 // Initialize by showing all content
-BOXS_CONTENT.forEach(content => content.classList.add('show'));
+BOXS_CONTENT.forEach((content) => content.classList.add("show"));
 
+// Function to change the image
+function changeImage(imgContainer) {
+  currentIndex = (currentIndex + 1) % getSelectedImages(imgContainer).length;
+  const img = imgContainer.querySelector("img");
+  img.src = BASE_IMG_URL + getSelectedImages(imgContainer)[currentIndex];
+}
 
+// Function to start the image loop on hover
+function startImageLoop(imgContainer) {
+  intervalId = setInterval(() => {
+    changeImage(imgContainer);
+  }, 1000);
+}
+
+// Function to stop the image loop when hover ends
+function stopImageLoop(imgContainer) {
+  clearInterval(intervalId);
+  const img = imgContainer.querySelector("img");
+  img.src = BASE_IMG_URL + getSelectedImages(imgContainer)[0]; // Reset to the first image
+  currentIndex = 0;
+}
+
+function getSelectedImages(imgContainer) {
+  let selectedImages = [];
+  const imgSrc = imgContainer.children[0].src;
+  if (imgSrc.includes("rally")) {
+    selectedImages = PROJECT_RALLY_IMGS;
+  } else if (imgSrc.includes("kendy")) {
+    selectedImages = PROJECT_KENDY_IMGS;
+  } else if (imgSrc.includes("zaabal")) {
+    selectedImages = PROJECT_ZAABAL_IMGS;
+  } else {
+    selectedImages = PROJECT_ZEEIN_IMGS;
+  }
+  return selectedImages;
+}
+
+// Event listeners for hover
+PROJECT_CONTAINERS.forEach((imgContainer) => {
+  imgContainer.addEventListener("mouseenter", () => {
+    startImageLoop(imgContainer);
+  });
+  imgContainer.addEventListener("mouseleave", () => {
+    stopImageLoop(imgContainer);
+  });
+});
